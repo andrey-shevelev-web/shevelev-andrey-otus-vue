d71c54ac5560caf05541cc6ed4a4d624b40ad2fd
Andrey Shevelev <andrey.shevelev.web@gmail.com>
2023-11-30 11:10:12 +0700

    order form validation refactoring

--- src/components/AppOrder.vue
+++ src/components/AppOrder.vue
@@ -1,62 +1,48 @@
 <script setup>
-import { reactive } from 'vue';
 import InputText from 'primevue/inputtext';
 import InputMask from 'primevue/inputmask';
 import Checkbox from 'primevue/checkbox';
 import Button from 'primevue/button';
-import Dropdown from 'primevue/dropdown';
 import { useForm } from 'vee-validate';
 import { postOrderForm } from '@/services/ProductService';
 
 const emit = defineEmits();
 
-function required(value) {
-  return value ? true : 'This field is required';
-}
+const emptyValidation = fieldName => value => {
+  return value ? true : `${fieldName} required`;
+};
 
-function requiredObjectValue(value) {
-  return value?.value ? true : 'This field is required';
-}
+const agreeWithRulesValidation = value => {
+  const notValidText = 'Your consent to the order processing rules is required';
+  return value ? true : notValidText;
+};
 
 const {
-  defineInputBinds,
   handleSubmit,
+  defineField,
   resetForm,
   errors: validationErrors
 } = useForm({
   validationSchema: {
-    firstname: required,
-    surname: required,
-    city: requiredObjectValue,
-    street: required,
-    house: required,
-    cardOwner: required,
-    cardNumber: required,
-    agreeWithRules: required
+    firstname: emptyValidation('First name'),
+    surname: emptyValidation('Surname'),
+    city: emptyValidation('City'),
+    street: emptyValidation('Street'),
+    house: emptyValidation('House'),
+    cardOwner: emptyValidation('Card owner'),
+    cardNumber: emptyValidation('Card number'),
+    agreeWithRules: agreeWithRulesValidation
   }
 });
 
-const firstname = defineInputBinds('firstname');
-const surname = defineInputBinds('surname');
-const city = defineInputBinds('city');
-const street = defineInputBinds('street');
-const house = defineInputBinds('house');
-const cardOwner = defineInputBinds('cardOwner');
-const cardNumber = defineInputBinds('cardNumber');
-const agreeWithRules = defineInputBinds('agreeWithRules');
-
-const cities = reactive([
-  { name: 'Москва', code: 'MSK' },
-  { name: 'Санкт Петербург', code: 'SP' },
-  { name: 'Новосибирск', code: 'NSK' },
-  { name: 'Екатеринбург', code: 'EKB' },
-  { name: 'Казань', code: 'KZN' },
-  { name: 'Нижний Новгород', code: 'NND' },
-  { name: 'Красноярск', code: 'KSK' },
-  { name: 'Челябинск', code: 'TCH' },
-  { name: 'Уфа', code: 'UF' },
-  { name: 'Ростов-на-Дону', code: 'RND' }
-]);
+const [firstname, firstnameAttrs] = defineField('firstname');
+const [surname, surnameAttrs] = defineField('surname');
+const [city, cityAttrs] = defineField('city');
+const [street, streetAttrs] = defineField('street');
+const [house, houseAttrs] = defineField('house');
+const [cardOwner, cardOwnerAttrs] = defineField('cardOwner');
+const [cardNumber, cardNumberAttrs] = defineField('cardNumber');
+const [agreeWithRules, agreeWithRulesAttrs] = defineField('agreeWithRules');
 
 const onSubmit = handleSubmit(async values => {
   const { firstname, surname, city, street, house, cardOwner, cardNumber } =
@@ -65,7 +51,7 @@ const onSubmit = handleSubmit(async values => {
   const formData = {
     firstname,
     surname,
-    city: city.value,
+    city,
     street,
     house,
     cardOwner,
@@ -73,6 +59,7 @@ const onSubmit = handleSubmit(async values => {
   };
 
   const response = await postOrderForm(formData);
+  resetForm();
   emit('orderFormSubmitted');
   emit('setHiddenOrderForm', true);
 });
@@ -84,52 +71,51 @@ const onSubmit = handleSubmit(async values => {
   >
     <div class="surface-card p-4 shadow-2 border-round w-full lg:w-4">
       <form @submit.prevent="onSubmit">
-        <div class="flex justify-content-between">
-          <div class="mb-3">
-            <label for="firstname-o1" class="block text-900 font-medium mb-2"
-              >Name</label
-            >
-            <InputText
-              id="firstname-o1"
-              type="text"
-              v-bind="firstname"
-              placeholder="Name"
-              :class="{ 'p-invalid': validationErrors.firstname }"
-            />
-            <small class="p-error">{{
-              validationErrors.firstname || '&nbsp;'
-            }}</small>
-          </div>
-
-          <div class="mb-3">
-            <label for="surname-o1" class="block text-900 font-medium mb-2"
-              >Surname</label
-            >
-            <InputText
-              id="surname-o1"
-              type="text"
-              v-bind="surname"
-              placeholder="Surname"
-              :class="{ 'p-invalid': validationErrors.surname }"
-            />
-            <small class="p-error">{{
-              validationErrors.surname || '&nbsp;'
-            }}</small>
-          </div>
+        <div class="mb-2">
+          <label for="firstname-o1" class="block text-900 font-medium mb-1"
+            >Name</label
+          >
+          <InputText
+            id="firstname-o1"
+            type="text"
+            v-model="firstname"
+            v-bind="firstnameAttrs"
+            placeholder="Name"
+            class="w-full"
+            :class="{ 'p-invalid': validationErrors.firstname }"
+          />
+          <small class="p-error">{{
+            validationErrors.firstname || '&nbsp;'
+          }}</small>
         </div>
 
-        <div class="mb-3">
-          <label for="city-o1" class="block text-900 font-medium mb-2"
+        <div class="mb-2">
+          <label for="surname-o1" class="block text-900 font-medium mb-1"
+            >Surname</label
+          >
+          <InputText
+            id="surname-o1"
+            type="text"
+            v-model="surname"
+            v-bind="surnameAttrs"
+            placeholder="Surname"
+            class="w-full"
+            :class="{ 'p-invalid': validationErrors.surname }"
+          />
+          <small class="p-error">{{
+            validationErrors.surname || '&nbsp;'
+          }}</small>
+        </div>
+
+        <div class="mb-2">
+          <label for="city-o1" class="block text-900 font-medium mb-1"
             >City / Town</label
           >
-          <Dropdown
+          <InputText
             id="city-o1"
             type="text"
-            v-bind="city"
-            v-model="modelValue"
-            :options="cities"
-            optionLabel="name"
-            optionValue="code"
+            v-model="city"
+            v-bind="cityAttrs"
             placeholder="City / Town"
             class="w-full"
             :class="{ 'p-invalid': validationErrors.surname }"
@@ -137,80 +123,83 @@ const onSubmit = handleSubmit(async values => {
           <small class="p-error">{{ validationErrors.city || '&nbsp;' }}</small>
         </div>
 
-        <div class="flex justify-content-between">
-          <div class="mb-3">
-            <label for="street-o1" class="block text-900 font-medium mb-2"
-              >Street</label
-            >
-            <InputText
-              id="street-o1"
-              type="text"
-              v-bind="street"
-              placeholder="Street"
-              :class="{ 'p-invalid': validationErrors.street }"
-            />
-            <small class="p-error">{{
-              validationErrors.street || '&nbsp;'
-            }}</small>
-          </div>
-
-          <div class="mb-3">
-            <label for="house-o1" class="block text-900 font-medium mb-2"
-              >House</label
-            >
-            <InputText
-              id="house-o1"
-              type="text"
-              v-bind="house"
-              placeholder="House"
-              :class="{ 'p-invalid': validationErrors.house }"
-            />
-            <small class="p-error">{{
-              validationErrors.house || '&nbsp;'
-            }}</small>
-          </div>
+        <div class="mb-2">
+          <label for="street-o1" class="block text-900 font-medium mb-1"
+            >Street</label
+          >
+          <InputText
+            id="street-o1"
+            type="text"
+            v-model="street"
+            v-bind="streetAttrs"
+            placeholder="Street"
+            class="w-full"
+            :class="{ 'p-invalid': validationErrors.street }"
+          />
+          <small class="p-error">{{
+            validationErrors.street || '&nbsp;'
+          }}</small>
         </div>
 
-        <div class="flex justify-content-between">
-          <div class="mb-3">
-            <label for="card-owner-o1" class="block text-900 font-medium mb-2"
-              >Card Owner</label
-            >
-            <InputText
-              id="card-owner-o1"
-              type="text"
-              v-bind="cardOwner"
-              placeholder="Card Owner"
-              :class="{ 'p-invalid': validationErrors.cardOwner }"
-            />
-            <small class="p-error">{{
-              validationErrors.cardOwner || '&nbsp;'
-            }}</small>
-          </div>
+        <div class="mb-2">
+          <label for="house-o1" class="block text-900 font-medium mb-1"
+            >House</label
+          >
+          <InputText
+            id="house-o1"
+            type="text"
+            v-model="house"
+            v-bind="houseAttrs"
+            placeholder="House"
+            class="w-full"
+            :class="{ 'p-invalid': validationErrors.house }"
+          />
+          <small class="p-error">{{
+            validationErrors.house || '&nbsp;'
+          }}</small>
+        </div>
 
-          <div class="mb-3">
-            <label for="card-number-o1" class="block text-900 font-medium mb-2"
-              >Card Number</label
-            >
-            <InputMask
-              id="card-number-o1"
-              v-bind="cardNumber"
-              v-model="modelValue"
-              mask="9999-9999-9999-9999"
-              placeholder="Card Number"
-              :class="{ 'p-invalid': validationErrors.cardNumber }"
-            />
-            <small class="p-error">{{
-              validationErrors.cardNumber || '&nbsp;'
-            }}</small>
-          </div>
+        <div class="mb-2">
+          <label for="card-owner-o1" class="block text-900 font-medium mb-1"
+            >Card Owner</label
+          >
+          <InputText
+            id="card-owner-o1"
+            type="text"
+            v-model="cardOwner"
+            v-bind="cardOwnerAttrs"
+            placeholder="Card Owner"
+            class="w-full"
+            :class="{ 'p-invalid': validationErrors.cardOwner }"
+          />
+          <small class="p-error">{{
+            validationErrors.cardOwner || '&nbsp;'
+          }}</small>
+        </div>
+
+        <div class="mb-2">
+          <label for="card-number-o1" class="block text-900 font-medium mb-1"
+            >Card Number</label
+          >
+          <InputMask
+            id="card-number-o1"
+            v-model="cardNumber"
+            v-bind="cardNumberAttrs"
+            mask="9999-9999-9999-9999"
+            placeholder="Card Number"
+            class="w-full"
+            :class="{ 'p-invalid': validationErrors.cardNumber }"
+          />
+          <small class="p-error">{{
+            validationErrors.cardNumber || '&nbsp;'
+          }}</small>
         </div>
 
         <div class="flex align-items-center mb-5">
           <Checkbox
             id="agree-with-rules-o1"
-            v-bind="agreeWithRules"
-            v-model="agreeWithRules.value"
+            v-model="agreeWithRules"
+            v-bind="agreeWithRulesAttrs"
             binary
             :class="{ 'p-invalid': validationErrors.agreeWithRules }"
           />
@@ -219,6 +208,7 @@ const onSubmit = handleSubmit(async values => {
             class="block text-900 font-medium ml-2"
             >I agree with the order processing rules</label
           >
+          <br />
           <small class="p-error">{{
             validationErrors.agreeWithRules || '&nbsp;'
           }}</small>
