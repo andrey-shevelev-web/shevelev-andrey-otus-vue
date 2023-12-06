388e466fa0e343561a28bba6ddce7d04c335db74
georgius1024 <georgius@ural.ru>
2023-12-06 22:11:30 +0500

    service specs improved

--- src/services/ProductService.spec.js
+++ src/services/ProductService.spec.js
@@ -1,13 +1,11 @@
 import { describe, it, expect } from "vitest";
 import * as testSubject from '@/services/ProductService'
-import mockProducts from '@/services/JsonProducts'
+import { jsonProducts } from '@/services/JsonProducts'
 import api from '@/services/api';
-vi.mock('@/services/api', () => ({
-  default:  {
-    get: vi.fn()
-  }
-}));
+
+
 describe('ProductService', () => {
+  const mockProducts = JSON.parse(jsonProducts)
   it('imports without errors', () => {
     expect(testSubject).toBeDefined()
     expect(testSubject).toBeTypeOf('object')
@@ -18,6 +16,6 @@ describe('ProductService', () => {
     api.get = vi.fn().mockResolvedValue({data: mockProducts})
     const products = await testSubject.getProducts()
     expect(products).toEqual(mockProducts)
-    expect(api.get).toBeCalled()
+    expect(api.get).toHaveBeenCalled()
   })
 })
